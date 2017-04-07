eefc6b390b10f4535bbac71ae68775f8f966e977
DUEC007 <DUEC007@GUTNB01283.nat.arvato-services.org>
2017-04-07 12:35:13 +0200

    added test

--- test/generator/JavaGeneratorTest.groovy
+++ test/generator/JavaGeneratorTest.groovy
@@ -1,13 +1,32 @@
 package generator
 
 import dmd.dsl.DSL
-import org.junit.Test
 
-class JavaGeneratorTest {
+import org.junit.After
+import org.junit.Assert
+import org.junit.Test
+import groovy.io.FileType
+
+class JavaGeneratorTest extends Assert{
+	
+	@After
+	public void tearDown() {
+		new File("src.dgen").deleteDir()
+	}
 
 	@Test
-	public void testGenerateJava() {
+	public void testGenerateJavaFiles() {
 		DSL script = new DSL()
 		String javaSource = script.generateJava(new File("test/resources"))
+		
+		new File("src.dgen").eachFileRecurse(FileType.FILES) { file ->
+			// all are java files
+			assertEquals("java", getExtention(file))
+		}
+	}
+	
+	
+	private String getExtention(File file){
+		file.name.substring(file.name.lastIndexOf('.') + 1)
 	}
 }
--- test/generator/SQLGeneratorTest.groovy
+++ test/generator/SQLGeneratorTest.groovy
@@ -1,13 +1,35 @@
 package generator
 
 import dmd.dsl.DSL
+import groovy.io.FileType
+
+import java.io.File
+import org.junit.After
+import org.junit.AfterClass
+import org.junit.Assert
 import org.junit.Test
 
-class SQLGeneratorTest {
+class SQLGeneratorTest extends Assert {
+	
+	@After
+	public void tearDown() {
+		new File("env/hsql/sql.dgen").deleteDir()
+		new File("env/hsql").delete()
+		new File("env").delete()
+	}
 
 	@Test
 	public void testGenerateSQL() {
 		DSL script = new DSL()
 		script.generateSQL(new File("test/resources"))
+		
+		new File("env/hsql/sql.dgen").eachFileRecurse(FileType.FILES) { file ->
+			// all are sql files
+			assertEquals("sql", getExtention(file))
+		}
+	}
+	
+	private String getExtention(File file){
+		file.name.substring(file.name.lastIndexOf('.') + 1)
 	}
 }
