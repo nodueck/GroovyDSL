92004ae17850f5a73086de8d1c17c158a18c94d7
DUEC007 <DUEC007@GUTNB01283.nat.arvato-services.org>
2017-04-29 17:48:03 +0200

    weitere Vereinfachungen des PlantUML Generators

--- src/generator/plantuml/PlantUmlGenerator.groovy
+++ src/generator/plantuml/PlantUmlGenerator.groovy
@@ -10,20 +10,30 @@ public class PlantUmlGenerator {
 	
 	
 	public static String model2PlantUml(DomainModel model){
-		def engine = new SimpleTemplateEngine(false) // set to true for verbose mode
-		def templateFilePath = this.getResource("templates/DomainModel.tmpl")
-		def template = engine.createTemplate(templateFilePath).make([domainModel: model, generator: PlantUmlGenerator])
-		return template.toString()
+		StringBuilder strBuilder = new StringBuilder();
+		
+		strBuilder.append("@startuml\n");
+		
+		model.domainObjects.each { 
+			strBuilder.append(domainAbstractionToPlantUml(it))
+		}
+		model.otherDomainAbstractions.each {
+			strBuilder.append(domainAbstractionToPlantUml(it))
+		}
+		
+		strBuilder.append("\n@enduml");
+		
+		return strBuilder.toString()
 	}
 	
-	public static String otherDomainAbstractionsToPlantUml(DomainAbstraction domainAbstr) {
+	public static String domainAbstractionToPlantUml(DomainAbstraction domainAbstr) {
 		def engine = new SimpleTemplateEngine(false) // set to true for verbose mode
 		def templateFilePath = this.getResource("templates/DomainAbstraction.tmpl")
 		def template = engine.createTemplate(templateFilePath).make([domainAbstr: domainAbstr, generator: PlantUmlGenerator])
 		return template.toString()
 	}
 	
-	
+	/** Tabulatorabstände aus der Notiz entfernen */ 
 	public static String getNote(DomainAbstraction domainAbstr){
 		if(domainAbstr.requirement){
 			String escaped = StringEscapeUtils.escapeJava(domainAbstr.requirement.content)
